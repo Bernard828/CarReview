cd5c4baed7c394425562fd589e4243fb4d1c64c7
Chuck R <rapmastercm@aol.com>
2020-06-25 20:30:59 -0400

    added Category repo,model, and controller

--- CarReview/CarBlogContext.cs
+++ CarReview/CarBlogContext.cs
@@ -10,6 +10,8 @@ namespace CarReview.Views
     public class CarBlogContext : DbContext
     {
         public DbSet<Content> CarList { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+
         //public DbSet<Review> Reviews { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -29,6 +31,14 @@ namespace CarReview.Views
                new Content("Used Car", 2, "My used car sucks", "Elon Musk", "Electric Car", DateTime.Now),
                new Content("Certfied Preowned", 3, "Not a good good", "Stradman", "Luxury Car", DateTime.Now),
                new Content("Salvage Title", 4, "Great project car", "DoctaM3", "Exotic Car", DateTime.Now)
+               );
+
+            modelbuilder.Entity<Category>().HasData(
+
+               new Category("Bernard", 1, "Car drives great. I love it."),
+               new Category("Jody", 2, "I've driven better cars. This car is subpar."),
+               new Category("Chuck", 3, "I'm a pro driver, and wouldn't drive a crappy car like this."),
+               new Category("Dede", 4, "I forgot my sunglasses and this car is a convertible.")
                );
         }
 }   }
---
+++ CarReview/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using CarReview.Models;
+using CarReview.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarReview.Repository
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        private CarBlogContext db;
+        public CategoryRepository(CarBlogContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return db.Category;
+        }
+
+        public Category GetById(int id)
+        {
+            return db.Category.Single(c => c.CategoryId == id);
+        }
+
+        public int Count()
+        {
+            return db.Category.Count();
+        }
+}   }
